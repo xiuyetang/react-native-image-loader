0f9c3d8fe3bda88814e4e8fb460fb5e129dbc7d7
Will Piers <wpiers@rallydev.com>
2015-03-31 09:36:36 -0600

    fix dumb bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function() {
   var path = this.resourcePath;
 
-  var pieces = str.split('/');
+  var pieces = path.split('/');
   var filePieces = pieces[pieces.length - 1].split('.');
   var name = filePieces[0];
 
